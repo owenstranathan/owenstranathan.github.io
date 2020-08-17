2a1bc5b91dbf5a3d6716a0c7753936a2b735d459
Owen Stranathan <owenstranathan@gmail.com>
2020-08-16 22:16:41 -0500

    Starting my own generator, with blackjack and hookers

---
+++ generator/generator.py
@@ -0,0 +1,77 @@
+import os
+import sys
+from collections import namedtuple
+
+from pathlib import Path
+
+from yaml import load, dump, load_all
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+Post = namedtuple("Post", ["source", "front", "body", "meta", "compiled"])
+
+def serialize_post(post_data):
+    # todo get front matter, parse it and put everything in a named tuple
+    blah = post_data.split("---")
+    if len(blah)>2:
+        front = blah[1]
+        body = "".join(blah[2:])
+    else:
+        front = None
+        body = post_data
+    try:
+        meta = next(load_all(post_data, Loader=Loader))
+    except Exception as e:
+        meta = None
+        print(e)
+    return Post(post_data, front, body, meta, "")
+
+
+def main(path):
+    if(os.path.exists(path)):
+        cwd = Path(os.path.abspath(path))
+    else:
+        cwd = Path(os.path.abspath(os.getcwd()))
+    site_conf = cwd / "site.yaml"
+    templates = cwd / "templates"
+    posts = cwd / "posts"
+    index = templates / "index.html"
+    if not index.exists():
+        print("can't work without an index.html")
+        sys.exit(-1)
+
+    if site_conf.exists():
+        with site_conf.open() as infstream:
+            data = load(infstream, Loader=Loader)
+    assert(templates.exists() and templates.is_dir())
+    assert(posts.exists() and posts.is_dir())
+    templates_dict = {}
+    posts_dict = {}
+    def read_file(f, dic, serializer = lambda d: d):
+        with f.open() as inf:
+            dic[str(f.absolute())] = serializer(inf.read())
+    def read_dir(d, dic, file_ext=None, serializer = lambda d: d):
+        for f in d.iterdir():
+            if f.is_file():
+                if file_ext is None:
+                    read_file(f, dic, serializer=serializer)
+                elif f.name.endswith(file_ext):
+                    read_file(f, dic, serializer=serializer)
+            else:
+                read_dir(f, dic, file_ext = file_ext, serializer=serializer)
+    read_dir(templates, templates_dict)
+    read_dir(posts, posts_dict, file_ext=".md", serializer=serialize_post)
+    # TODO for each post compile it with the global data from site.yaml and it's front matter
+    # then compile all the templates and output their corresponding pages
+    # NOTE: we're using mako so you'll need to update the template lang in the templates and post, which a bummer for sure
+    for x in posts_dict.values():
+        print(x.meta)
+
+
+if __name__ == "__main__":
+    if(len(sys.argv) > 1):
+        main(sys.argv[1])
+    else:
+        main()
